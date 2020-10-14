a221a68e8615b80d50cbb3b530f9f55f8cd29287
paikia01 <1226447825@qq.com>
2020-10-14 18:45:02 +0800

    Update install_build_toolchain.sh

--- Btools/build_toolchain/install_build_toolchain.sh
+++ Btools/build_toolchain/install_build_toolchain.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-:<<EOF
+#:<<EOF
 DESTDIR=/opt/rt-n56u/toolchain
 DL_NAME="out.tar"
-DL_URL="https://github.com/hminid/Build_Padavan_toolchain-3.4.x/releases/download/20200527/$DL_NAME"
+DL_URL="https://github.com/paikia01/Build_Padavan_toolchain-3.4.x/releases/download/20201014/$DL_NAME"
+#DL_URL="https://github.com/hminid/Build_Padavan_toolchain-3.4.x/releases/download/20200527/$DL_NAME"
 #
 curl -O -L $DL_URL && \
 mkdir -p $DESTDIR && \
 tar -xvf $DL_NAME -C $DESTDIR
 #tar -xvf out.tar -C /opt/rt-n56u/toolchain
-EOF
+#EOF
 
-cd /opt/rt-n56u/toolchain
-sudo ./clean_sources.sh
-sudo ./build_toolchain.sh
+#cd /opt/rt-n56u/toolchain
+#sudo ./clean_sources.sh
+#sudo ./build_toolchain.sh
